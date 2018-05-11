38da57f316411cf9c9d11d11a2e4a9cf60d20a2e
Radu Ciorba <rciorba@bitdefender.com>
2018-05-11 22:15:43 +0300

    add server dir

---
+++ server/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="spam-server",
+    version="0.0.1",
+    platforms='any',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[],
+    author="Artists formerly known as spam",
+    author_email="radu@devrandom.ro",
+    description="",
+    entry_points={
+        # 'console_scripts': [
+        #     'mongo_2_es = importer.mongo:main',
+        # ]
+    },
+
+    classifiers=[
+    ],
+)
