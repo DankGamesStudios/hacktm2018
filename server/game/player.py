2d3e1c2dcae5a7e90765f1499f8afa07ab3cfd2f
Ava-O <octavia.stancu@gmail.com>
2018-05-12 14:55:09 +0300

    player movement

--- server/game/player.py
+++ server/game/player.py
@@ -1,5 +1,8 @@
 """ Module for player logic. This could include the AI as well. """
+from exceptions import IllegalMoveException
+import grid
 
+MOVE_LIMITS = range(-2, 2)
 
 class Player(object):
     """ Class to model business logic of player.
@@ -8,7 +11,7 @@ class Player(object):
         print("creating player")
         self.health = 100
         self.name = name
-        self.position = position
+        self.position = list(position)
     
     def __str__(self):
         return "{name}[{health}] at ({x}, {y})".format(
@@ -17,3 +20,14 @@ class Player(object):
             x=self.position[0],
             y=self.position[0]
         )
+
+    def move(self, horizontal_pos, vertical_pos):
+        if horizontal_pos in MOVE_LIMITS and vertical_pos in MOVE_LIMITS:
+            if self.position[0] < grid.GRID_HEIGHT and self.position[1] < grid.GRID_WIDTH:
+                self.position[0] += horizontal_pos
+                self.position[1] += vertical_pos
+            else:
+                raise IllegalMoveException("Player position outside grid range")
+        else:
+            raise IllegalMoveException("Player moves not in range {}".format(MOVE_LIMITS))
+        return self.position
