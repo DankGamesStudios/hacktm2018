74cfc21d0ffa1ecc7277d81ba21b96abe9df9f0f
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2018-05-12 18:21:58 +0300

    Move client state in manager.

--- client/src/components/manager.js
+++ client/src/components/manager.js
@@ -1,25 +1,83 @@
-import io from 'socket.io-client';
+const CREATE_PLAYER_ID = 'CREATE_PLAYER_ID';
+const Action = {
+    CREATE_PLAYER_ID,
+};
+const NONE = 'NONE';
+const LASER = 'LASER';
+const SHIELD = 'SHIELD';
+export const Tile = {
+    NONE,
+    LASER,
+    SHIELD,
+};
 
 export default class GameManager {
 
+
     constructor() {
-        var s = new WebSocket('ws://10.10.2.16:8000/');
+        this.socket = this.createSocket();
+        this.availablePlayers = 0;
+        this.players = [{
+            name: 'You',
+            x: 4,
+            y: 3,
+            health: 100,
+        }, {
+            name: 'thelegend27',
+            status: 'waiting',
+            x: 10,
+            y: 2,
+            health: 90,
+        }];
+        this.myIndex = 0;
+        this.round = 0;
+        this.phase = 'select';
+        this.lastRow = 5;
+        this.rows = {
+            5: [Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE],
+            4: [Tile.NONE, Tile.SHIELD, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE],
+            3: [Tile.LASER, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE],
+            2: [Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE],
+            1: [Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE, Tile.NONE],
+        };
+    }
+
+    getSelectableTiles() {
+        let player = this.players[this.myIndex];
+        return {
+            minX: player.x - 2,
+            maxX: player.x + 2,
+            minY: player.y - 2,
+            maxY: player.y + 2,
+        };
+    }
+
+    createSocket() {
+        let s = new WebSocket('ws://10.10.2.16:8000/');
         s.addEventListener('error', function (m) {
-            log("error");
+            console.log("error");
         });
         s.addEventListener('open', function (m) {
             console.log("websocket connection open");
             s.send(JSON.stringify({
-                action: "CREATE_PLAYER_ID"
+                action: Action.CREATE_PLAYER_ID
             }));
         });
         s.addEventListener('message', function (m) {
             console.log(m.data);
         });
-        this.availablePlayers = 1;
+        return s;
     }
 
     getAvailablePlayers() {
         return this.availablePlayers;
+    }
+
+    getStatus() {
+        let status = {
+            round: this.round,
+            phase: this.phase,
+        };
+        return status;
     }
 }
--- client/src/states/game.js
+++ client/src/states/game.js
@@ -3,22 +3,25 @@ import p2 from 'expose-loader?p2!phaser-ce/build/custom/p2.js';
 import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js';
 
 import VisualTimer from '../components/timer';
+import {Tile} from '../components/manager';
 
 // Phaser x low->high === left->right
 // Phaser y low->high === top->down
 
 export default class Game extends Phaser.State {
 
-    constructor() {
-        super();
+    constructor(game, manager) {
+        super(game);
+        this.manager = manager;
+
         this.board_margin_x = 100;
         this.board_margin_y = 170;
         this.padding_x = 20;
         this.padding_y = 20;
         this.tile_size = 60;
         this.row_size = 100;
-        this.move_rows = false;
         this.selectedTile = null;
+        this.roundText = null;
         //TODO: use arrow functions, but webpack/babel did not cooperate
         this.selectNextTile = this.selectNextTile.bind(this);
 
@@ -32,81 +35,104 @@ export default class Game extends Phaser.State {
         this.nr_rows = 5;
         this.nr_columns = 12;
         this.players = {};
-        this.nr_players = 0;
         this.timer = null;
-    }
-
-    getSelectedVersion(currentVersion) {
-        return {
-            'plus': 'minus',
-            'minus': 'plus'
-        }[currentVersion];
     }
 
     preload(game) {
 
     }
 
-    addPlayer(name, xTile, yTile, key = 'player') {
-        let tile = this.rows[yTile][xTile];
+    // position between 0 and 3
+    addPlayer(name, xTile, yTile, position, key = 'player') {
+        let tile = this.rows[yTile][xTile].tile;
         let sprite = this.game.add.sprite(tile.x, tile.y, key);
         sprite.anchor.setTo(0.5, 0.5);
         sprite.animations.add('run');
         let nameSprite = this.game.add.text(
-            this.player_start_x + this.nr_players * this.player_spacing_x,
+            this.player_start_x + position * this.player_spacing_x,
             this.player_start_y,
             name,
             {font: '30px', fill: '#9eff63', align: 'center'});
         nameSprite.anchor.set(0.5, 0.5);
         let healthSprite = this.game.add.text(
-            this.player_start_x + this.nr_players * this.player_spacing_x,
+            this.player_start_x + position * this.player_spacing_x,
             this.player_start_y + 70,
             '9000+',
             {font: '30px', fill: '#9eff63', align: 'center'});
         healthSprite.anchor.set(0.5, 0.5);
+        let extraSprite = this.game.add.text(
+            this.player_start_x + position * this.player_spacing_x,
+            this.player_start_y + 140,
+            '',
+            {font: '30px', fill: '#9eff63', align: 'center'});
+        extraSprite.anchor.set(0.5, 0.5);
         this.players[name] = {
-            id: this.nr_players,
             name,
             nameSprite,
             healthSprite,
+            extraSprite,
             sprite,
             xTile,
             yTile,
-        }
-        this.nr_players++;
+        };
     }
 
     selectNextTile(selectedTile) {
         if (this.selectedTile) {
-            this.selectedTile.loadTexture(this.getSelectedVersion(this.selectedTile.key));
+            this.selectedTile.loadTexture('normal_tile');
         }
         console.log('selected tile changed', selectedTile);
-        selectedTile.loadTexture(this.getSelectedVersion(selectedTile.key));
+        selectedTile.loadTexture('normal_tile_selected');
         this.selectedTile = selectedTile;
     }
 
-    createTile(x, y, type = 'plus') {
-        let tile = this.game.add.sprite(x, y, type);
+    createTile(x, y, type) {
+        // console.log(type, Tile.LASER);
+        let tile = this.game.add.sprite(x, y, 'normal_tile');
+        let power = null;
+        if (type === Tile.LASER) {
+            power = this.game.add.sprite(x, y, 'laser');
+        } else if (type === Tile.SHIELD) {
+            power = this.game.add.sprite(x, y, 'shield');
+        }
+        if (power) {
+            power.anchor.setTo(0.5, 0.5);
+        }
         tile.anchor.setTo(0.5, 0.5);
         tile.inputEnabled = true;
         tile.input.useHandCursor = true;
         tile.events.onInputDown.add(this.selectNextTile, this);
-        return tile;
+        return {
+            tile,
+            power,
+        };
     }
 
-    create(game) {
+    drawBoard() {
+        this.rows.map(row => {
+            row.map(tileData => tileData.tile.destroy());
+        });
+        this.rows = [];
         for (let row = 0; row < this.nr_rows; row++) {
             let new_row = [];
+            let rowData = this.manager.rows[this.manager.lastRow - row];
+            // console.log(rowData);
             for (let col = 0; col < this.nr_columns; col++) {
                 let x = this.padding_x + col * this.row_size + this.board_margin_x;
                 let y = this.padding_y + row * this.row_size + this.board_margin_y;
-                new_row.push(this.createTile(x, y));
+                new_row.push(this.createTile(x, y, rowData[col]));
             }
             this.rows.push(new_row);
         }
-        this.setNewRowState({});
-        this.addPlayer('Player1', 4, 3);
-        this.addPlayer('Player2', 10, 3);
+    }
+
+    create(game) {
+        this.drawBoard();
+        // this.setNewRowState({});
+        for (let i = 0; i < this.manager.players.length; i++) {
+            let player = this.manager.players[i];
+            this.addPlayer(player.name, player.x, player.y, i);
+        }
         this.timer = new VisualTimer({
             game: this.game,
             x: 120,
@@ -116,18 +142,23 @@ export default class Game extends Phaser.State {
                 console.log('timer completed')
             }
         });
+        this.roundText = this.game.add.text(
+            600,
+            100,
+            'Round 0',
+            {font: '30px', fill: '#9eff63', align: 'center'});
         console.log('Game state');
     };
 
-    setNewRowState(stateFromServer) {
-        console.log('Something happened on the server, let\'s update rows');
-        this.move_rows = true;
-        window.setTimeout(() => {
-            this.setNewRowState({});
-            this.timer.reset();
-            this.timer.start();
-        }, 5000);
-    }
+    // setNewRowState(stateFromServer) {
+    //     console.log('Something happened on the server, let\'s update rows');
+    //     this.move_rows = true;
+    //     window.setTimeout(() => {
+    //         this.setNewRowState({});
+    //         this.timer.reset();
+    //         this.timer.start();
+    //     }, 5000);
+    // }
 
     addNewRow() {
         let removedRow = this.rows.splice(-1, 1)[0];
@@ -160,38 +191,70 @@ export default class Game extends Phaser.State {
         // }
     }
 
-    updatePlayers() {
-        let player1 = this.players['Player1'];
-        player1.sprite.bringToTop();
-        if (this.selectedTile) {
-            player1.sprite.angle = this.setDirection(player1.sprite, this.selectedTile);
-            let newLocation = {x: this.selectedTile.x, y: this.selectedTile.y + this.row_size};
-            player1.sprite.animations.play('run', 15, true);
-            // this.game.add.tween(player1.sprite).to(newLocation, 200, 'Bounce', true);
-            let animationTime = 1000;
-            this.game.add.tween(player1.sprite).to(newLocation, animationTime, Phaser.Easing.Bounce.Out, true, 0);
-            //TODO find other way to cancel animation
-            window.setTimeout(() => {
-                    player1.sprite.animations.stop(true);
-                },
-                animationTime);
-        }
+    updatePlayer(player, destination) {
+        player.sprite.bringToTop();
+        player.sprite.angle = this.setDirection(player.sprite, destination);
+        let newLocation = {x: destination.x, y: destination.y + this.row_size};
+        player.sprite.animations.play('run', 15, true);
+        let animationTime = 1000;
+        this.game.add.tween(player.sprite).to(newLocation, animationTime, Phaser.Easing.Bounce.Out, true, 0);
+        //TODO find other way to cancel animation
+        window.setTimeout(() => {
+                player.sprite.animations.stop(true);
+            },
+            animationTime);
     }
 
     update(game) {
-        if (this.move_rows) {
-            // console.log('moving rows', this.rows);
-            for (let rIndex = 0; rIndex < this.rows.length; rIndex++) {
-                let row = this.rows[rIndex];
-                for (let tileIndex = 0; tileIndex < row.length; tileIndex++) {
-                    let tile = row[tileIndex];
-                    this.game.add.tween(tile).to({y: '+' + this.row_size}, 300, 'Bounce', true);
+        let status = this.manager.getStatus();
+        // console.log('status', status);
+        let prompt = '';
+        if (status.phase === 'select') {
+            prompt = 'Please select action';
+        } else if (status.phase === 'waiting') {
+            prompt = 'Waiting on other players';
+        }
+        this.roundText.text = 'Round ' + status.round + ', ' + prompt;
+        for (let i = 0; i < this.manager.players.length; i++) {
+            let player = this.manager.players[i]
+            let playerUI = this.players[player.name];
+            let extraText = '';
+            if (player.status === 'waiting') {
+                extraText = 'Slowpoke!';
+            } else if (status.phase === 'done') {
+                let newTile = this.rows[player.y][player.x];
+                this.updatePlayer(playerUI, newTile)
+            }
+            this.players[player.name].extraSprite.text = extraText;
+            this.players[player.name].healthSprite.text = player.health;
+        }
+        let available = this.manager.getSelectableTiles();
+        for (let i = 0; i < this.rows.length; i++) {
+            let row = this.rows[i];
+            for (let col = 0; col < row.length; col++) {
+                if (!this.selectedTile && col >= available.minX && col <= available.maxX && i >= available.minY && i <= available.maxY) {
+                    row[col].tile.loadTexture('normal_tile_available');
+                } else if (this.selectedTile == row[col].tile) {
+                    row[col].tile.loadTexture('normal_tile_selected');
+                } else {
+                    row[col].tile.loadTexture('normal_tile');
                 }
             }
-            this.addNewRow();
-            this.move_rows = false;
-            this.updatePlayers();
-            // console.log('rows moved');
         }
+
+        // if (this.move_rows) {
+        //     // console.log('moving rows', this.rows);
+        //     for (let rIndex = 0; rIndex < this.rows.length; rIndex++) {
+        //         let row = this.rows[rIndex];
+        //         for (let tileIndex = 0; tileIndex < row.length; tileIndex++) {
+        //             let tile = row[tileIndex];
+        //             this.game.add.tween(tile).to({y: '+' + this.row_size}, 300, 'Bounce', true);
+        //         }
+        //     }
+        //     this.addNewRow();
+        //     this.move_rows = false;
+        //     this.updatePlayers();
+        //     // console.log('rows moved');
+        // }
     };
 };
