21c9c9ee7c3c7b77bc5dce3e52579cfbb0b7c8e2
Virtosu Bogdan <virtosu.bogdan@gmail.com>
2018-05-12 10:51:56 +0300

    Cycle rows.

--- client/src/states/game.js
+++ client/src/states/game.js
@@ -6,53 +6,93 @@ export default class Game extends Phaser.State {
 
     constructor() {
         super();
-        this.rows = [];
+        this.rows = []; // Top (0) -> Down (max - 1)
+        this.board_margin_x = 70;
+        this.board_margin_y = 70;
         this.padding_x = 20;
         this.padding_y = 20;
         this.tile_size = 60;
         this.row_size = 100;
+        this.nr_rows = 5;
+        this.nr_columns = 12;
         this.move_rows = false;
+        this.selectedTile = null;
+        //TODO: use arrow functions, but webpack/babel did not cooperate
+        this.selectNextTile = this.selectNextTile.bind(this);
+    }
+
+    getSelectedVersion(currentVersion) {
+        return {
+            'plus': 'minus',
+            'minus': 'plus'
+        }[currentVersion];
     }
 
     preload(game) {
 
     }
 
+    selectNextTile(selectedTile) {
+        if (this.selectedTile) {
+            this.selectedTile.loadTexture(this.getSelectedVersion(this.selectedTile.key));
+        }
+        console.log('selected tile changed', selectedTile);
+        selectedTile.loadTexture(this.getSelectedVersion(selectedTile.key));
+        this.selectedTile = selectedTile;
+    }
+
+    createTile(x, y, type = 'plus') {
+        let tile = this.game.add.sprite(x, y, type);
+        tile.anchor.setTo(0.5, 0.5);
+        tile.inputEnabled = true;
+        tile.input.useHandCursor = true;
+        tile.events.onInputDown.add(this.selectNextTile, this);
+        return tile;
+    }
+
     create(game) {
-        for (let row = 0; row < 5; row++) {
+        for (let row = 0; row < this.nr_rows; row++) {
             let new_row = [];
-            for (let col = 0; col < 8; col++) {
-                let x = this.padding_x + col * this.row_size;
-                let y = this.padding_y + row * this.row_size;
-                let tile = this.game.add.sprite(x, y, 'plus');
-                tile.anchor.setTo(0.5, 0.5);
-                new_row.push(tile);
+            for (let col = 0; col < this.nr_columns; col++) {
+                let x = this.padding_x + col * this.row_size + this.board_margin_x;
+                let y = this.padding_y + row * this.row_size + this.board_margin_y;
+                new_row.push(this.createTile(x, y));
             }
             this.rows.push(new_row);
         }
-        let timeoutID = window.setTimeout(() => {
-            console.log('Something happened on the server, let\'s update rows');
-            this.move_rows = true;
-        }, 3000);
+        this.setNewRowState({});
         console.log('Game state');
     };
 
+    setNewRowState(stateFromServer) {
+        console.log('Something happened on the server, let\'s update rows');
+        this.move_rows = true;
+        window.setTimeout(() => {
+            this.setNewRowState({});
+        }, 5000);
+    }
+
     addNewRow() {
         let removedRow = this.rows.splice(-1, 1)[0];
-        removedRow.map(tile => tile.destroy());
+        removedRow.map(tile => {
+            if (this.selectedTile === tile) {
+                this.selectedTile = null;
+            }
+            tile.destroy();
+        });
         let newRow = [];
-        for (let col = 0; col < 8; col++) {
-            let x = this.padding_x + col * this.row_size;
-            let y = this.padding_y;
-            let tile = this.game.add.sprite(x, y, 'minus');
-            tile.anchor.setTo(0.5, 0.5);
-            newRow.push(tile);
+        for (let col = 0; col < this.nr_columns; col++) {
+            let x = this.padding_x + col * this.row_size + this.board_margin_x;
+            let y = this.padding_y + this.board_margin_y;
+            newRow.push(this.createTile(x, y, 'minus'));
         }
-        this.rows.push(newRow);
+        this.rows.splice(0, 0, newRow);
+        console.log('adding new row', this.rows);
     }
 
     update(game) {
         if (this.move_rows) {
+            console.log('moving rows', this.rows);
             for (let rIndex = 0; rIndex < this.rows.length; rIndex++) {
                 let row = this.rows[rIndex];
                 for (let tileIndex = 0; tileIndex < row.length; tileIndex++) {
