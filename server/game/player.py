e8427231701969215fa50ef6d818411d4b37afa7
Ava-O <octavia.stancu@gmail.com>
2018-05-12 16:03:21 +0300

    corrected range, check pos before move

--- server/game/player.py
+++ server/game/player.py
@@ -2,7 +2,7 @@
 from exceptions import IllegalMoveException
 from grid import GRID_HEIGHT, GRID_WIDTH
 
-MOVE_LIMITS = range(-2, 2)
+MOVE_LIMITS = range(-3, 3)
 
 class Player(object):
     """ Class to model business logic of player.
@@ -12,7 +12,7 @@ class Player(object):
         self.health = 100
         self.name = name
         self.position = list(position)
-        self.check_initial_position()
+        self.valid_position()
     
     def __str__(self):
         return "{name}[{health}] at ({x}, {y})".format(
@@ -22,13 +22,19 @@ class Player(object):
             y=self.position[1]
         )
 
-    def check_initial_position(self):
-        if self.position[0] > GRID_HEIGHT or self.position[1] > GRID_WIDTH:
+    def valid_position(self):
+        if not self.position_in_grid_range(self.position[0], self.position[1]):
             raise IllegalMoveException("Player position outside grid range")
+        return True
+
+    def position_in_grid_range(self, row_coord, col_coord):
+        return row_coord <= GRID_HEIGHT or col_coord <= GRID_WIDTH
 
     def move(self, horizontal_pos, vertical_pos):
         if horizontal_pos in MOVE_LIMITS and vertical_pos in MOVE_LIMITS:
-            if self.position[0] < GRID_HEIGHT and self.position[1] < GRID_WIDTH:
+            horizontal_coord = self.position[0] + horizontal_pos
+            vertical_coord = self.position[1] + vertical_pos
+            if self.position_in_grid_range(horizontal_coord, vertical_coord):
                 self.position[0] += horizontal_pos
                 self.position[1] += vertical_pos
             else:
