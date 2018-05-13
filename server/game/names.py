8e98e7bcbabd32cab9db6df0afe8c2dd27b62431
Ava-O <octavia.stancu@gmail.com>
2018-05-13 15:57:30 +0300

    ensure player names are unique in a game

--- server/game/names.py
+++ server/game/names.py
@@ -3,8 +3,17 @@ import random
 from .name_choices import NAMES, ADJECTIVES
 
 
+USED_NAMES = []
+
+
 def get_name():
     name = random.choice(NAMES)
     adjective = random.choice(ADJECTIVES)
     nb = random.randint(100, 999)
-    return '{} {}-{}'.format(adjective, name, nb)
+    player_name = '{} {}-{}'.format(adjective, name, nb)
+    if player_name not in USED_NAMES:
+        USED_NAMES.append(player_name)
+        return player_name
+    else:
+        return get_name()
+
