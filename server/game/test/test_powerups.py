62fe7b3cd43f6a8499d15b052ff9efa51a8cf5d6
Ava-O <octavia.stancu@gmail.com>
2018-05-13 13:32:58 +0300

    test hammer powerup

--- server/game/test/test_powerups.py
+++ server/game/test/test_powerups.py
@@ -2,12 +2,17 @@ from game import powerups
 from game.game import Game
 
 
-def test_laser():
+def create_game():
     # preconditions
     game = Game()
     game.start()
     # default players are in a row
     game.add_default_players()
+    return game
+
+
+def test_laser():
+    game = create_game()
     test_obj = powerups.Laser()
     iterable = iter(game.players)
     # first player uses laser
@@ -15,3 +20,18 @@ def test_laser():
     # meaning second player suffers damage
     player2 = game.players[next(iterable)]
     assert player2.health == 100 - test_obj.damage
+
+
+def test_hammer():
+    game = create_game()
+    test_obj = powerups.Hammer()
+    iterable = iter(game.players)
+    first_player = game.players[next(iterable)]
+    test_obj.activate(game, first_player)
+    health_sum = 0
+    for player in game.players.values():
+        print(player.health)
+        health_sum += player.health
+    assert health_sum < 400
+    assert health_sum == 375
+    assert first_player.health == 100
