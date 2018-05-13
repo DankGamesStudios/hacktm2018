89bcc2790a96a62ac151cd7230b3042a37908c6b
Ava-O <octavia.stancu@gmail.com>
2018-05-13 16:00:05 +0300

    removed nb from name to avoid having equal names with diff nb

--- server/game/names.py
+++ server/game/names.py
@@ -9,8 +9,7 @@ USED_NAMES = []
 def get_name():
     name = random.choice(NAMES)
     adjective = random.choice(ADJECTIVES)
-    nb = random.randint(100, 999)
-    player_name = '{} {}-{}'.format(adjective, name, nb)
+    player_name = '{} {}'.format(adjective, name)
     if player_name not in USED_NAMES:
         USED_NAMES.append(player_name)
         return player_name
